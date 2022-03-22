6b830865fa07c54002eb7b26f90bb11b0352ae25
Mayte Araujo <maypg@bol.com.br>
2022-03-22 00:00:59 -0300

    Bootstrap & Layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.master')
+@section('content')
+
+      <!-- Header -->
+
+          <section id="header">
+              <div class="inner">
+                  <span class="icon solid major fa-cloud"></span>
+                  <h1> <strong>Cidade Verde</strong><br /></h1>
+                  <p>Reuze, Reduza, Recicle</p>
+                  <ul class="actions special">
+                      <li><a href="#one" class="button scrolly">Iniciar</a></li>
+                  </ul>
+              </div>
+          </section>
+
+      <!-- One -->
+          <section id="one" class="main style1">
+              <div class="container">
+                  <div class="row gtr-100">
+                      <div class="col-6 col-12-medium">
+                          <header class="major">
+                              <h2>Quem Somos<br /></h2>
+                          </header>
+                          <p>Somos uma associação sem fins lucrativos, com o objetivo de evitar o descarte de materiais inservíveis e itens recicláveis, que não possuem mais utilidade, em locais irregulares causando transtornos urbanos como obstruções na rede de drenagem, aspecto de local sujo, além de servir como criadouros de insetos. Indicamos estabelecimentos que recebem Vidros, plásticoas, metais, madeiras, pilhas, óleo de cozinha, pneus, restos de móveis, dentre outros itens.</p>
+                      </div>
+                      <div class="col-6 col-12-medium imp-medium">
+                          <span class="image fit"><img src="assets/images/fundo.png" alt="" /></span>
+                      </div>
+                  </div>
+              </div>
+          </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/ecoponto', function () {
+    return view('site.ecoponto');
+})->name('site.ecoponto');
+
+Route::get('/contato', function () {
+    return view('site.contato');
+})->name('site.contato');
